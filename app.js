f8d0b64ff6d0c8e2d9b6ccdab94c8a361124418e
raphanus <phil.wpark+github@gmail.com>
2015-01-23 14:42:52 -0500

    Added descriptions

--- app.js
+++ app.js
@@ -7,20 +7,27 @@
 
     var works = [
     {
-        title: 'Trexquant',
-        subtitle: 'Trexquant Data Tabulator',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi suscipit diam vel magna fermentum, in porttitor purus convallis. Phasellus venenatis et ex sit amet euismod. Morbi vel quam vitae magna aliquam finibus vel at erat. Vivamus eu sodales dolor. Etiam et maximus ex. Quisque dignissim nibh porta tellus blandit, a dictum ligula fringilla. In tempus mollis fermentum.',
+        title: 'Trexquant Data Tabulator',
+        subtitle: 'Fast, Reliable Representation of Performance',
+        desc: "Responding to the need for information to be easily and quickly understood, I developed an internal PHP/Javascript application to visualize financial data from Bloomberg. Our firm’s daily and long-term profits and losses are graphed and tracked. The data tabulator uses a series of methods—such as heat maps and graphs—to display the performance of individual financial strategies that are being traded within the company's portfolio."
     },
     {
         title: 'Trade Simulator',
         subtitle: 'Intra Day Trade Simulations',
-        desc: 'Quick project developed in MATLAB that incorporated the existing simulation framework',
+        desc: "At TrexQuant, I created an Intra-day simulator that modeled profits and losses from trading stocks within a 12-hour period using MATLAB. The results provided substantive information to evaluate and test certain alpha ideas. "
     },
     {
         title: 'GRE Vocab Quizzer',
-        subtitle: 'Intelligent Vocabulary',
-        desc: 'hi hi hi',
-    }];
+        subtitle: 'Smarter Questions, Smarter Answers',
+        desc: 'A Django application designed to help students study for the GRE test, this vocabulary quizzer was designed to respond to the strengths and weaknesses of each student. By remembering and focusing on the words that students struggle with, adaptability becomes a crucial element to this application. Similar quizzers randomly generate answers and fail to enhance the user’s performance. To address this issue, I applied Levenshtein distances to develop a system that generates answer choices that are similar to each other, thus increasing the user’s need for attention. '
+
+    },
+    {
+        title: 'MATLAB Mesh Generator',
+        subtitle: 'Easy Meshes for the Uninitiated',
+        desc: "I participated in research project led by Dr. Anne Fausto-Sterling that explored whether or not mothers inherently preferred certain interactions with their children based on gender. Although I initially used MATLAB and SPSS to conduct statistical analysis, I eventually converted the data into an interface that could be used by those who were unfamiliar with such programs. The GUIDE toolbox allowed users to easily visualize represented data and increased the process’ efficiency by 300%, providing a smooth transition after I graduated. "
+        }
+    ];
 
     var links = [{
             title: 'ABOUT',
